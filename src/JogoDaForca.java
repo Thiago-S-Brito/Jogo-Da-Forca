272e53cfdead0b3332e8d8d356a65f4a33b479ad
Thiago-S-Brito <thhiago369@gmail.com>
2023-11-02 13:19:39 -0300

    adição das palavras periferico e headset ao jogo

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -6,7 +6,7 @@ public class JogoDaForca {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String[] palavras = {"programacao", "java", "computador", "desenvolvimento", "inteligencia", "hardware", "gabinete", "monitor", "codificação", "mousepad", "teclado",
-        "webcam"};
+        "webcam", "periferico", "headset"};
 
         String palavraSecreta = palavras[random.nextInt(palavras.length)];
         int tentativasMaximas = 6;
