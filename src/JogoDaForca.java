58e74afa1e0b6865dbe7dc71fa076f5167d356dc
Thiago-S-Brito <thhiago369@gmail.com>
2023-11-07 16:57:40 -0300

    adição das palavras minimizar e maximizar, ao jogo

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -6,7 +6,7 @@ public class JogoDaForca {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String[] palavras = {"programacao", "java", "computador", "desenvolvimento", "inteligencia", "hardware", "gabinete", "monitor", "codificação", "mousepad", "teclado",
-        "webcam", "periferico", "headset", "ventoinha", "software", "controle", "arquivo", "aplicativo", "navegador", "compactar", "descompactar"};
+        "webcam", "periferico", "headset", "ventoinha", "software", "controle", "arquivo", "aplicativo", "navegador", "compactar", "descompactar", "minimixar", "maximizar"};
 
         String palavraSecreta = palavras[random.nextInt(palavras.length)];
         int tentativasMaximas = 6;
